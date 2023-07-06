4caaa4ef1cd33f01e7062d5b1accaf1a09522cbe
nspsck <86347287+nspsck@users.noreply.github.com>
2023-07-07 00:59:25 +0200

    Add files via upload

---
+++ OCTestMultiThread.py
@@ -0,0 +1,184 @@
+import machine, time, random, _thread
+from micropython import const
+from machine import Pin
+
+"""
+This test assumes that your board has a led on the pin 25.
+
+Notes:
+1. The Pico can only run the second thread on core1. The Pico is
+only capble running 2 threads atm.
+
+2. The temperature sensor is not accurate by default. for
+details please visit: https://youtu.be/YeY0SY8iYxk?t=132
+Or check the official datasheet yourself. (Chapter: 4.9.5)
+https://datasheets.raspberrypi.com/rp2040/rp2040-datasheet.pdf
+
+3. Note the lock for frequencies is obsolete. I just added it for
+fun. Why only use one lock if you can have two. C:
+PS. It actually slows the other thread down for a tiny little bit.
+
+"""
+
+_BASE_FREQUENCY = const(125_000_000)
+_REST_FREQUENCY = const(20_000_000) #unused
+
+led = Pin(25, Pin.OUT)
+count = 0
+ledStart = 0
+ledEnd = 0
+temperature = 0
+turbo_freq = 0
+run_count = 0
+
+lock = _thread.allocate_lock()
+freqlock = _thread.allocate_lock()
+
+def is_valid_freq(freq):
+    freq *= 1000000
+    try:
+        machine.freq(freq)
+    except ValueError as e:
+        return False
+    return True
+
+
+def find_clock_freq(limit):
+    for i in range(limit):
+        if is_valid_freq(i):
+            print("%d MHz is valid" % i)
+#     return [x for x in range(limit) if is_valid_freq(x)]
+    
+
+
+def print_info(count, end, start, temperature, id):
+    print("Round: %d, time used: %d ms, done by thread %d." % (count, end - start, id))
+    temperature = 27- (machine.ADC(4).read_u16() * 3.3 / (65535)- 0.706)/0.001721
+    print("Current frequency: %dMHz Temperature: {:.2f}".format(round(temperature, 2)) % (machine.freq() // 1000000))
+    
+
+def stresstest_vanilla(random):
+    for i in range(0,100000):
+        random.random() * random.random()
+        
+        
+def stresstest_led(ledEnd, ledStart, random):
+    for x in range(0,100000):
+        random.random() * random.random()
+        #toggle LED to verify runtime
+        ledEnd = time.ticks_ms()
+        if (ledEnd - ledStart >= 500):
+            led.toggle()
+            ledStart = time.ticks_ms()
+    return ledEnd, ledStart
+
+
+def change_frequency(freq):
+    freqlock.acquire()
+    machine.freq(freq)
+    freqlock.release()
+    
+
+def stresstest(random, functionID):
+    global count
+    global ledEnd
+    global ledStart
+    
+    #calculate the time used by running the stress test
+    change_frequency(turbo_freq)
+    print("Current frequency (OC): %dMHz" % (machine.freq() // 1000000))
+    start = time.ticks_ms()
+    if (functionID == 0):
+        stresstest_vanilla(random)
+    elif (functionID == 1):
+        ledEnd, ledStart = stresstest_led(ledEnd, ledStart, random)
+    else:
+        pass
+    end = time.ticks_ms()
+    change_frequency(_BASE_FREQUENCY)
+    
+    #output the result and the condition of the chip
+    lock.acquire()
+    count = count + 1
+    print_info(count, end, start, temperature, functionID)
+    lock.release()
+    
+
+def test_thread_non_stop():
+    global count
+    count = 0
+    while True:
+        stresstest(random, 1)
+        
+
+def run_non_stop(freq):
+    global turbo_freq
+    global count
+    count = 0
+    
+    # Test to see if the frequency is supported
+    turbo_freq = int(freq * 1000000)
+    try:
+        change_frequency(turbo_freq)
+    except ValueError as e:
+        freqlock.release()
+        print("ValueError: can not change frequency to %dMHz" % (turbo_freq // 1000000))
+        print("Aborted.")
+        return e
+
+    _thread.start_new_thread(test_thread_non_stop, ())
+    while True:
+        stresstest(random, 0)
+        
+        
+def test_thread():
+    global run_count
+    global count
+    
+    # reset for next run    
+    run_count = 0
+    count = 0
+    
+    while True:
+        lock.acquire()
+        run_count += 1
+        lock.release()
+        if run_count > 100:
+            break 
+        stresstest(random, 1)
+            
+
+def run(freq):
+    global turbo_freq
+    global count
+    global run_count
+    
+    # reset for next run    
+    run_count = 0
+    count = 0
+    
+    # Test to see if the frequency is supported
+    turbo_freq = int(freq * 1000000)
+    try:
+        change_frequency(turbo_freq)
+    except ValueError as e:
+        freqlock.release()
+        print("ValueError: can not change frequency to %dMHz" % (turbo_freq // 1000000))
+        print("Aborted.")
+        return e
+    
+    print("The test goes for 100 rounds total.")
+    _thread.start_new_thread(test_thread, ())
+    
+    while True:
+        lock.acquire()
+        run_count += 1
+        lock.release()
+        if run_count > 100:
+            break  
+        stresstest(random, 0)
+        
+
+
+
+
---
+++ POV.py
@@ -0,0 +1,145 @@
+import machine
+from micropython import const
+
+"""
+Useful cheat sheets:
+
+# Content in _VREG_AND_CHIP_RESET_BASE (0x40064000)
+ROK = const(0x00001000) #[12] ROK (0): regulation status
+VSEL = const(0x000000f0) #[7:4] VSEL (0xb): output voltage select
+HIZ = const(0x00000002) #[1] HIZ (0): high impedance mode select
+EN = const(0x00000001) #[0] EN (1): enable
+
+Everything else is unused.
+
+"""
+
+class VoltageError(Exception):
+    pass
+
+
+# The voltage and chip reset control address
+_VREG_AND_CHIP_RESET_BASE = const(0x40064000)
+
+# Possible voltage
+_VREG_VOLTAGE_0_85 = const(0b0110)    #< 0.85v
+_VREG_VOLTAGE_0_90 = const(0b0111)    #< 0.90v
+_VREG_VOLTAGE_0_95 = const(0b1000)    #< 0.95v
+_VREG_VOLTAGE_1_00 = const(0b1001)    #< 1.00v
+_VREG_VOLTAGE_1_05 = const(0b1010)    #< 1.05v
+_VREG_VOLTAGE_1_10 = const(0b1011)    #< 1.10v
+_VREG_VOLTAGE_1_15 = const(0b1100)    #< 1.15v
+_VREG_VOLTAGE_1_20 = const(0b1101)    #< 1.20v
+_VREG_VOLTAGE_1_25 = const(0b1110)    #< 1.25v
+_VREG_VOLTAGE_1_30 = const(0b1111)    #< 1.30v
+
+# The masks to extract/replace the voltage settings
+_CLEAN_VSEL_VALUE_MASK = const(0xffffff0f)
+_CLEAN_RAMDOM_BITS_MASK = const(0x000000f0)
+
+
+def read_mem(mem_addr):
+    return machine.mem32[mem_addr
+]
+
+
+def voltage_control_bits(volt_bits):
+    return (volt_bits << 4) & _CLEAN_RAMDOM_BITS_MASK
+
+
+def clean_vsel_bits():
+    return read_mem(_VREG_AND_CHIP_RESET_BASE) & _CLEAN_VSEL_VALUE_MASK
+
+
+def isclose(a, b):
+    if abs(a - b) < 0.004:
+        return True
+    else:
+        return False
+    
+
+def set_voltage_bits(volt):
+
+    if isclose(volt, 0.85):
+        return clean_vsel_bits() ^ voltage_control_bits(_VREG_VOLTAGE_0_85)
+
+    elif isclose(volt, 0.90):
+        return clean_vsel_bits() ^ voltage_control_bits(_VREG_VOLTAGE_0_90)
+
+    elif isclose(volt, 0.95):
+        return clean_vsel_bits() ^ voltage_control_bits(_VREG_VOLTAGE_0_95)
+
+    elif isclose(volt, 1.00):
+        return clean_vsel_bits() ^ voltage_control_bits(_VREG_VOLTAGE_1_00)
+
+    elif isclose(volt, 1.05):
+        return clean_vsel_bits() ^ voltage_control_bits(_VREG_VOLTAGE_1_05)
+
+    elif isclose(volt, 1.10):
+        return clean_vsel_bits() ^ voltage_control_bits(_VREG_VOLTAGE_1_10)
+
+    elif isclose(volt, 1.15):
+        return clean_vsel_bits() ^ voltage_control_bits(_VREG_VOLTAGE_1_15)
+
+    elif isclose(volt, 1.20):
+        return clean_vsel_bits() ^ voltage_control_bits(_VREG_VOLTAGE_1_20)
+
+    elif isclose(volt, 1.25):
+        return clean_vsel_bits() ^ voltage_control_bits(_VREG_VOLTAGE_1_25)
+
+    elif isclose(volt, 1.30):
+        return clean_vsel_bits() ^ voltage_control_bits(_VREG_VOLTAGE_1_30)
+
+    else:
+        raise ValueError("Unsupported inputs. Valid inputs has to be close to: 0.85 ~ 1.30, with a 0.05 increment each step. Voltage unchanged.")
+
+
+def set_voltage(volt):
+    try:
+        machine.mem32[_VREG_AND_CHIP_RESET_BASE] = set_voltage_bits(volt)
+        return True
+    except ValueError as e:
+        print("Error: ", str(e))
+        return False
+    
+
+def test_non_stop(freq):
+    if freq >= 1000:
+        print("The unit used is MHz, please try again.")
+        return
+    elif freq <= 0:
+        print("The input must be positiv")
+        return
+    try:
+        import OCTestMultiThread as OC
+        OC.run_non_stop(freq)
+    except ImportError as e:
+        print("ImportError", str(e))
+        print("You can get the test on: ")
+        
+
+def test(freq):
+    if freq >= 1000:
+        print("The unit used is Mhz, please try again.")
+        return
+    elif freq <= 0:
+        print("The input must be positiv")
+        return
+    try:
+        import OCTestMultiThread as OC
+        OC.run(freq)
+    except ImportError as e:
+        print(str(e))
+        print("You can get the test on: ")
+        
+
+def find_valid_clocks(limit):
+    try:
+        import OCTestMultiThread as OC
+        OC.find_clock_freq(limit)
+    except ImportError as e:
+        print(str(e))
+        print("You can get the test on: ")
+        
+                
+                
